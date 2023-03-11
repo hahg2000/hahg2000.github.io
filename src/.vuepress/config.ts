9f31b0a466011e3760990a724f9f6bca8e1a90f2
hahg2000 <61403802+hahg2000@users.noreply.github.com>
2023-03-11 19:06:19 +0800

    测试部署

--- src/.vuepress/config.ts
+++ src/.vuepress/config.ts
@@ -4,19 +4,9 @@ import theme from "./theme.js";
 export default defineUserConfig({
   base: "/",
 
-  locales: {
-    "/": {
-      lang: "en-US",
-      title: "Blog Demo",
-      description: "A blog demo for vuepress-theme-hope",
-    },
-    "/zh/": {
-      lang: "zh-CN",
-      title: "博客演示",
-      description: "vuepress-theme-hope 的博客演示",
-    },
-  },
-
+  // 设置正在使用的语言
+  lang: "zh-CN",
+  
   theme,
 
   // Enable it with pwa
